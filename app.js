6f4de73086aba93ae912a199f8904c12380a01ce
RaczekNieBoRaczek <wertyksonred@gmail.com>
2022-08-15 19:44:44 +0200

    update

--- app.js
+++ app.js
@@ -1,8 +1,59 @@
-const cardArray = [
-  {
-    name: 'fries',
-    img: 'images/fries.png',
+const cardArray = [{
+  name: 'fries',
+  img: 'images/fries.png'
 
-  }
+},
+{
+  name: 'cheeseburger',
+  img: 'images/cheeseburger.png'
 
-]
+},
+{
+  name: 'pizza',
+  img: 'images/pizza.png'
+
+},
+{
+  name: 'hotdog',
+  img: 'images/hotdog.png'
+
+}, {
+  name: 'ice-cream',
+  img: 'images/ice-cream.png'
+
+}, {
+  name: 'milkshake',
+  img: 'images/pizza.png'
+
+},
+{
+  name: 'fries',
+  img: 'images/fries.png'
+
+},
+{
+  name: 'cheeseburger',
+  img: 'images/cheeseburger.png'
+
+}, {
+  name: 'pizza',
+  img: 'images/pizza.png'
+
+},
+{
+  name: 'hotdog',
+  img: 'images/hotdog.png'
+
+},
+{
+  name: 'ice-cream',
+  img: 'images/ice-cream.png'
+
+},
+{
+  name: 'milkshake',
+  img: 'images/pizza.png'
+
+}]
+
+cardArray.sort(() => 0.5 - Math.random())
